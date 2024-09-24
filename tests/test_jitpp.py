f477437c8898c12190437bb7ec0c85e655bcbd2e
Fabrice Normandin <normandf@mila.quebec>
2024-09-24 10:14:04 -0400

    Add test to check PEP 563 support
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

---
+++ tests/test_jitpp.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import jax
+from xtils import jitpp
+from xtils.jitpp import Static
+import pytest
+
+
+def foo(
+    a: jax.Array,
+    b: Static[bool],
+):
+    if b:
+        return a + 1
+    return a
+
+
+def test_jitpp_with_future_annotations():
+    a = jax.numpy.array([1, 2, 3])
+
+    def check(fn):
+        assert fn(a, True).tolist() == [2, 3, 4]
+        assert fn(a, False).tolist() == [1, 2, 3]
+
+    # works:
+    check(jax.jit(foo, static_argnames=["b"]))
+
+    # doesn't work:
+    with pytest.raises(jax.errors.TracerBoolConversionError):
+        check(jitpp.jit(foo))
